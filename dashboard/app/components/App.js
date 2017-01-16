dc7b1566f01023ca3b9c83a0fc824ae3cf93b7cf
dorianb <dorian.bagur@gmail.com>
2017-01-16 16:53:12 +0100

    Docker

--- dashboard/app/components/App.js
+++ dashboard/app/components/App.js
@@ -2,7 +2,9 @@ var React = require('react');
 var NavBar = require('./NavBar');
 var Home = require('./Home');
 
-// http://elections.huffingtonpost.com/2016/results/president
+// http://elections.huffingtonpost.com/2016/results/presidenth
+// https://www.tutorialspoint.com/reactjs/reactjs_using_flux.htm
+
 var App = React.createClass({
 
   render: function () {
