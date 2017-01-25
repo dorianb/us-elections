60769503bead45cbc791b17dfcfa3f5d50d03401
dorianb <dorian.bagur@gmail.com>
2017-01-25 12:11:04 +0100

    Sending date

--- dashboard/app/api/DjangoApi.js
+++ dashboard/app/api/DjangoApi.js
@@ -4,18 +4,20 @@ var MapViewActions = require('../actions/MapViewActions');
 var axios = require('axios');
 
 var URL = "http://127.0.0.1";
-var start_time = new Date("November 08, 2016 20:00:00").toISOString();
-
+var time = new Date("November 08, 2016 21:10:00");
+var timer = setInterval(function() {
+  time.setSeconds(time.getSeconds() + 1)
+}, 1000);
 
 var ElectionsAPI = function() {
 
     this.getSummary = function() {
       var url = URL + '/summary';
       //var url = '../app/data/summary.json';
-      console.log("Request: " + url + " at " + start_time);
+      console.log("Request: " + url + " at " + time);
       axios.get(url, {
         params: {
-          start_time: start_time
+          start_time: time
         }
       }).then(function(data) {
         console.log(data);
@@ -26,10 +28,10 @@ var ElectionsAPI = function() {
     this.getMapInfo = function() {
       //var url = URL + '/timeline';
       var url = '../app/data/timeLine.json';
-      console.log("Request: " + url + " at " + start_time);
+      console.log("Request: " + url + " at " + time);
       axios.get(url, {
         params: {
-          start_time: start_time
+          start_time: time
         }
       }).then(function(data) {
         console.log(data);
@@ -38,12 +40,12 @@ var ElectionsAPI = function() {
     };
 
     this.getMapView = function() {
-      //var url = URL + '/mapview';
+      //var url = URL + '/map';
       var url = '../app/data/results.json';
-      console.log("Request: " + url + " at " + start_time);
+      console.log("Request: " + url + " at " + time);
       axios.get(url, {
         params: {
-          start_time: start_time
+          start_time: time
         }
       }).then(function(data) {
         console.log(data);
--- dashboard/app/components/Header.js
+++ dashboard/app/components/Header.js
@@ -1,13 +1,41 @@
 var React = require('react');
 
 var Header = React.createClass({
-
+  getDefaultProps: function() {
+    return {};
+  },
+  getInitialState: function() {
+    return {
+      timing: new Date("November 08, 2016 20:10:00")
+    };
+  },
+  componentWillMount: function() {
+    if (this._timer) {
+      clearInterval(this._timer);
+      this._timer = null;
+    }
+    var self = this;
+    this._timer = setInterval(function() {
+      var date = self.state.timing
+      date.setSeconds(date.getSeconds() + 1)
+      self.setState({timing: date});
+    }, 1000);
+  },
+  componentsWillUnmount: function() {
+    if (this._timer) {
+      clearInterval(this._timer);
+      this._timer = null;
+    }
+  },
   render: function () {
     return (
       <header>
         <h2 className="election-header">
           <span className="dem">Election</span>
           <span className="gop">2016</span>
+        </h2>
+        <h2 className="election-header">
+          <span>{this.state.timing.toLocaleTimeString()}</span>
         </h2>
       </header>
     );
